9a2450be3e38ced541c46b2625a18cf961babd27
vijaykumar <eng23am0085@dsu.edu.in>
2025-06-18 19:06:54 +0530

    added now

--- backend/index.js
+++ backend/index.js
@@ -3,13 +3,12 @@ const dotenv = require('dotenv');
 const urlRoutes = require('./routes/urlRoutes');
 const connectDB = require('./db'); 
 const cors = require('cors');
+
 dotenv.config();
 
 const app = express();
-
-// Enable CORS for your frontend BEFORE routes
 app.use(cors({
-  origin: "https://short-url-webapp-pjsh.vercel.app/", 
+  origin: "https://short-url-webapp-pjsh.vercel.app",
   methods: ["GET", "POST"],
   credentials: true,
 }));
@@ -19,9 +18,10 @@ app.use(express.json());
 connectDB();
 
 app.use('/', urlRoutes);
-app.get("/",(req,res)=>{
+
+app.get("/", (req, res) => {
   res.send("Welcome to the URL Shortener API");
-})
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
